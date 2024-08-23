fd8f95946c8829937ae189460674fa382c5e1382
Santiago Muñoz <santiagomunozruiz@gmail.com>
2024-08-22 19:50:54 -0600

    Intentando algo

--- thingies.js
+++ thingies.js
@@ -2,8 +2,8 @@ document.addEventListener("DOMContentLoaded", function() {
     const navbar = document.querySelector("nav");
     Array.from(navbar.children).forEach(function(link) {
         console.log(link)
-        console.log(window.location.href)
-        if (window.location.href == link.getAttribute("href")) {
+        console.log(window.location.pathname)
+        if (window.location.pathname == link.getAttribute("href")) {
             link.className = "active";
         }
     });
