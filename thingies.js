e2a33eb75314a9023a53a630392189167d732e3e
Santiago Muñoz <santiagomunozruiz@gmail.com>
2024-08-22 19:56:38 -0600

    Ya mejor así lo dejo

--- thingies.js
+++ thingies.js
@@ -1,10 +1,3 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const navbar = document.querySelector("nav");
-    Array.from(navbar.children).forEach(function(link) {
-        const full_link = `/Santis-Blog${link}`;
-        console.log(full_link)
-        if (window.location.pathname == link.getAttribute("href")) {
-            link.className = "active";
-        }
-    });
+    
 });
